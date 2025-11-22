8a443ddcfe35697cd80be788acdbcc4a5b9edd83
nolanmyles20 <nolanmyles@icloud.com>
2025-11-22 05:31:33 -0500

    Update scoring.js

--- js/scoring.js
+++ js/scoring.js
@@ -98,7 +98,7 @@ const POPUP_IMAGES_CONFIG_URL = 'config/popup_images.json';
 let popupImagesConfig = {
   strike: [],
   spare: [],
-  gutter: ["BowlingCenter/images/IMG_5219.gif"]
+  gutter: ["images/IMG_5219.gif"]
 };
 
 let bowlingPopupTimeout = null;
